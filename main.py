0dde4c73a5c6fca7ae1eb602951157f8c046c21f
Yaroslav Dytko <yarosdytko@gmail.com>
2023-02-07 15:03:43 +0100

    initial commit added imports

---
+++ main.py
@@ -0,0 +1,11 @@
+import shutil
+import tkinter as tk
+from tkinter import *
+from tkinter import messagebox, filedialog
+
+# main window
+root = tk.Tk()
+root.title("CopyMoveGUI")
+
+
+root.mainloop()
